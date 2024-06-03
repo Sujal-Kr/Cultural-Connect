9c7897d541159463119390194d3ef0302d13d1f8
Sujal-Kr <sunnykumarcms45@gmail.com>
2024-06-04 01:59:04 +0530

    "Slight ui changed"

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import Home from "./components/Home"
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/translate" exact element={<Translate/>}/>
-      <Route path="/quiz" exact element={<Quiz/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/translate" exact element={<Translate />} />
+        <Route path="/quiz" exact element={<Quiz />} />
+      </Routes>
 
     </>
   )
--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -77,10 +77,10 @@ const TranslateComponent = () => {
     };
 
     return (
-        <div className="max-w-4xl mx-auto p-6">
+        <div className="max-w-4xl mx-auto p-6 h-full min-h-screen flex flex-col justify-center">
             <h1 className="text-2xl font-bold text-center mb-8">Text Translator</h1>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">From</label>
                     <select
