359a064b731749edb06852a1f465f7d27efd8606
Sujal-Kr <sunnykumarcms45@gmail.com>
2024-06-04 00:25:55 +0530

    "Commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import Translate from "./components/Translate"
-import Quizes from "./components/Quizes"
+import Quiz from "./components/Quiz"
+import Home from "./components/Home"
 
 const App = () => {
   return (
     <>
     <Routes>
+      <Route path="/" element={<Home/>}/>
       <Route path="/translate" exact element={<Translate/>}/>
-      <Route path="/quizes" exact element={<Quizes/>}/>
+      <Route path="/quiz" exact element={<Quiz/>}/>
     </Routes>
 
     </>
